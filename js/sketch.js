d841d6be32d099c205b70db20ec952d3ce0b4858
Kellen Garrett <kellengarrett96@gmail.com>
2016-12-09 22:06:30 -0800

    Added reset button

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,31 @@
 $(document).ready(function(){
-  createGrid();
-  $(".grid").hover(function(){
-    $(this).addClass("highlighted");
-  })
+  createGrid(4);
 });
 
-function createGrid(){
-  var gridDiv;
-  for (var i = 0; i < 16; i++) {
-    gridDiv = "<div class='grid' id='" + i + "'></div>"
-    $(".wrapper").append(gridDiv);
+function colorGrid(){
+  $(".grid").hover(function(){
+    $(this).css('background-color', 'green');
+  })
+};
+
+function createGrid(sketchSize){
+  var sketchDiv;
+  var gridSize = 800/sketchSize;
+  var $sketchPad = $(".sketchPad")
+  for (var i = 0; i < sketchSize * sketchSize; i++) {
+    $sketchPad.append("<div class='grid'></div>");
   };
-}
+  $(".grid").css({"width" : gridSize + "px", "height": gridSize + "px"})
+  colorGrid();
+};
+
+function reset() {
+  $(".grid").remove(".grid");
+  var userSize = prompt("Pick a grid width between 1 and 64");
+  if (userSize >= 1 && userSize <= 64) {
+    createGrid(userSize);
+  } else {
+    alert("You did not choose a number between 1 and 64")
+    createGrid(4);
+  }
+};
